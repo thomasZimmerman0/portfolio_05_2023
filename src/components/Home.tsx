ef368c9e04c315d0e11c700094f8a5096320ec09
Tom Zimmerman <you@example.com>
2023-07-16 19:32:39 -0400

    removed unnecessary lines from Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,32 +9,6 @@ import PersonalProjects from "./PersonalProjects";
 
 function Home() {
 
-  const titles: string[] = [
-    "Software Developer",
-    "Engineer", 
-    "Leader", 
-    "Analytical Problem Solver", 
-    "Compasionate Friend"
-  ]
-  const [currentTitle, setCurrentTitle] = useState<{
-    indexTitle: number,
-    indexWord: number,
-    title: string
-  }>({
-    indexTitle: 0,
-    indexWord: 0,
-    title: ""
-  })
-
-  useEffect(()=>{
-    setTimeout(()=>{
-      setCurrentTitle((prevState)=>{
-        return prevState
-      })
-    },100)
-  }, [currentTitle])
-
-
   return (
     <div className="home-main-container">
       <section className="home-banner">
